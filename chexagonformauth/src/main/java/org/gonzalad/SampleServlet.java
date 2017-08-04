456fa95a4ea0901fe8fb5141ba0ca21bae8622c8
gonzalad <adr_gonzalez@yahoo.fr>
2017-08-04 14:14:03 +0200

    Tomcat Form Authentication
    Sample for test form authentication with tomcat
    *and*
    chexagon/redis-session-manager
    
    This illustrates issue
    https://github.com/chexagon/redis-session-manager/issues/14

---
+++ chexagonformauth/src/main/java/org/gonzalad/SampleServlet.java
@@ -0,0 +1,33 @@
+package org.gonzalad;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class SampleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("This is the Test Servlet");
+
+        out.print("<br/>Principal: <em>" + request.getUserPrincipal());
+
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = (String) headerNames.nextElement();
+            out.print("<br/>Header Name: <em>" + headerName);
+            String headerValue = request.getHeader(headerName);
+            out.print("</em>, Header Value: <em>" + headerValue);
+            out.println("</em>");
+        }
+    }
+
+}
